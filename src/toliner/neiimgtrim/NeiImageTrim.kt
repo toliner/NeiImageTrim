01d7efac46247cb7d472f95e6c129ad7c6996811
トリナー <toliner.rkt@gmail.com>
2018-08-23 22:59:46 +0900

    filter処理の改善と、それに伴ったパイプライン処理の改善

--- src/toliner/neiimgtrim/NeiImageTrim.kt
+++ src/toliner/neiimgtrim/NeiImageTrim.kt
@@ -12,11 +12,16 @@ fun main(args: Array<String>) {
     }
     File(".").walk()
             .filter {
-                it.isFile && it.extension == "png" && it.parent == File(".").parent
-            }.flatMap { file ->
-                ImageIO.read(file).run {
-                    sequenceOf(FilePic(file, getSubimage(304, 110, 240, 120)), FilePic(file, getSubimage(304, 240, 240, 120)))
-                }
+                it.isFile && it.extension == "png"
+            }.map { file ->
+                FilePic(file, ImageIO.read(file))
+            }.filter { data ->
+                data.image.width == 854 && data.image.height == 480
+            }.flatMap { data ->
+                sequenceOf(
+                        FilePic(data.file, data.image.getSubimage(304, 110, 240, 120)),
+                        FilePic(data.file, data.image.getSubimage(304, 240, 240, 120))
+                )
             }.forEachIndexed { index, data ->
                 data.image.flush()
                 File("./recipe-image/${data.file.name}-$index.png").run {
