c9bcbcf016dbeecd4d3e249ca4f89da16b0d40ec
トリナー <toliner.rkt@gmail.com>
2018-09-01 18:12:19 +0900

    パイプラインを改良

--- src/toliner/neiimgtrim/NeiImageTrim.kt
+++ src/toliner/neiimgtrim/NeiImageTrim.kt
@@ -18,18 +18,17 @@ fun main(args: Array<String>) {
             }.filter { data ->
                 data.image.width == 854 && data.image.height == 480
             }.flatMap { data ->
+                val fileName = "./recipe-image/${data.file.name}"
                 sequenceOf(
-                        data.copy(image = data.image.getSubimage(304, 110, 240, 120)),
-                        data.copy(image = data.image.getSubimage(304, 240, 240, 120))
+                        FilePic(File("$fileName-0.png"), data.image.getSubimage(304, 110, 240, 120)),
+                        FilePic(File("$fileName-1.png"), data.image.getSubimage(304, 240, 240, 120))
                 )
-            }.forEachIndexed { index, data ->
+            }.filter {
+                !it.file.exists()
+            }.forEach { data ->
                 data.image.flush()
-                File("./recipe-image/${data.file.name}-$index.png").run {
-                    if (!exists()) {
-                        createNewFile()
-                        ImageIO.write(data.image, "png", this)
-                    }
-                }
+                data.file.createNewFile()
+                ImageIO.write(data.image, "png", data.file)
             }
 }
 
